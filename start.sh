73f235f9b157334aad12abf419f0850c3a470bec
sebvx <117357460+sebvx@users.noreply.github.com>
2022-11-03 15:49:45 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # Создание разделов диска
 lsblk
 read -p "Disk =  " AddDisk
-echo -e "n\np\n1\n\n+1024M\nn\np\n2\n\n\na\n1\nw\n" | fdisk /dev/$AddDisk
+echo -e "n\np\n1\n\n+824M\nn\np\n2\n\n\na\n1\nw\n" | fdisk /dev/$AddDisk
 #Форматирование разделов
 mkfs.fat -F32 -n BOOT /dev/sda1
 mkfs.btrfs -f -L ROOT /dev/sda2
@@ -12,25 +12,22 @@ mount /dev/sda2 /mnt
 #Создаём BTRFS тома
 btrfs su cr /mnt/@
 btrfs su cr /mnt/@home
-btrfs su cr /mnt/@root
-btrfs su cr /mnt/@var
-btrfs su cr /mnt/@opt
 btrfs su cr /mnt/@tmp
 btrfs su cr /mnt/@snapshots
 #Отмантируем раздел
 umount /dev/sda2
 #Монтируем тома в разделы со сжатием и свойствами
 mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@ /dev/sda2  /mnt/
-mkdir -p /mnt/{boot/EFI,home,root,var,opt,tmp,.snapshots}
+mkdir -p /mnt/{boot/EFI,home,tmp,.snapshots}
 mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@home /dev/sda2  /mnt/home
-mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@root /dev/sda2  /mnt/root
-mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@var /dev/sda2  /mnt/var
-mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@opt /dev/sda2  /mnt/opt
 mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@snapshots /dev/sda2  /mnt/.snapshots
 mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@tmp /dev/sda2  /mnt/tmp
 mount dev/sda1 /mnt/boot/EFI
 #Установка минимального набора
 sed 's/#ParallelDownloads = 5/ParallelDownloads = 10/g' -i /etc/pacman.conf
+echo 'Server = https://archlinux.astra.in.ua/$repo/os/$arch' >> etc/pacman.d/mirrorlist
+echo 'Server = https://fastmirror.pp.ua/archlinux/$repo/os/$arch' >> etc/pacman.d/mirrorlist
+echo 'Server = http://mirrors.nix.org.ua/linux/archlinux/$repo/os/$arch' >> etc/pacman.d/mirrorlist
 pacman -Syy --noconfirm
 pacstrap /mnt base base-devel btrfs-progs linux-zen linux-zen-headers linux-zen-docs linux-firmware grub grub-btrfs os-prober efibootmgr dosfstools mtools 
 #Генерация fstab
